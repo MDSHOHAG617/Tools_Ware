915a91a1defaef78fc51c97035de7cef25164112
MDSHOHAG617 <mdshohag119764@gmail.com>
2022-05-22 02:57:04 +0600

    footer added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Banner from "./Banner";
 import BusinessSummary from "./BusinessSummary";
+import Footer from "./Footer";
 import Tools from "./Tools";
 
 const Home = () => {
@@ -9,6 +10,8 @@ const Home = () => {
       <Banner></Banner>
       <Tools></Tools>
       <BusinessSummary></BusinessSummary>
+
+      <Footer></Footer>
     </div>
   );
 };
