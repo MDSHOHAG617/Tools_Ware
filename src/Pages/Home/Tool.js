4cdf071ae6d2403c1c2dc9bea2a811f03bc70557
MDSHOHAG617 <mdshohag119764@gmail.com>
2022-05-22 01:46:47 +0600

    tools route added

---
+++ src/Pages/Home/Tool.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Tool = ({ tool }) => {
+  return (
+    <div>
+      <p> tools : {tool}</p>
+    </div>
+  );
+};
+
+export default Tool;
---
+++ src/Pages/Home/Tools.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import Tool from "./Tool";
+
+const Tools = () => {
+  const [tools, setTools] = useState("");
+  console.log(tools);
+  useEffect(() => {
+    fetch("http://localhost:5000/tool")
+      .then((res) => res.json())
+      .then((data) => setTools(data));
+  }, []);
+  return (
+    <div>
+      tools : {tools.name}
+      {tools.map((tool) => console.log(tool))}
+    </div>
+  );
+};
+
+export default Tools;
