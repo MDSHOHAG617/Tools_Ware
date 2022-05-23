17bcc72cc8fe7e9785369d059160396f7b0cd665
MDSHOHAG617 <mdshohag119764@gmail.com>
2022-05-23 20:19:56 +0600

    myorders display

--- src/Pages/DashBoard/MyOrders.js
+++ src/Pages/DashBoard/MyOrders.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const MyOrders = () => {
-  return <div>my orders</div>;
+  const [orders, setOrders] = useState([]);
+  const [user] = useAuthState(auth);
+
+  useEffect(() => {
+    if (user) {
+      fetch(`http://localhost:5000/order?customerEmail=${user.email}`)
+        .then((res) => res.json())
+        .then((data) => setOrders(data));
+    }
+  }, [user]);
+
+  return (
+    <div>
+      my orders :{orders.length}
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>product</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <tr>
+                <th>1</th>
+                <td>{order.customerName}</td>
+                <td>{order.customerEmail}</td>
+                <td>{order.tool}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyOrders;
